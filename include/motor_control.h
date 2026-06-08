aadf1b967c0fceb5ba25f2a6f124df4414a85d18
RomnKho <rkhokho@upv.edu.es>
2026-06-08 10:00:41 +0200

    Fixed handles and added set_duty function

--- include/motor_control.h
+++ include/motor_control.h
@@ -9,4 +9,10 @@
 /** 
  * @brief   Initializes the needed submodules for MCPWM
  **/
-void init_mcpwm(void); 
+void mcpwm_init(void); 
+
+/**
+ * @brief       Sets the duty cycle for the PWM => If per_duty is not in range, it does not do anything
+ * @param[in]   per_duty => Integer in range [0, 100]
+ **/
+void mcpwm_set_duty(uint8_t per_duty);
--- src/motor_control.c
+++ src/motor_control.c
@@ -4,7 +4,7 @@
  * @author  Roman Khomenko
  **/
 
-/* ====== INCLUDES ============== */
+/* ========= INCLUDES =========== */
 
 #include "driver/gpio.h"
 #include "driver/mcpwm_timer.h"
@@ -14,28 +14,35 @@
 #include "esp_log.h"
 #include "../include/motor_control.h"
 
-/* ====== DEFINES =============== */
+/* ========== DEFINES =========== */
 
 #define     MCPWM_GROUP_ID      0
 #define     MCPWM_CLK_SOURCE    MCPWM_TIMER_CLK_SRC_DEFAULT
 #define     MCPWM_CLK_HZ        1000000                     // 1 MHz
 #define     MCPWM_FREQ          2000
+#define     MCPWM_PERIOD_TICKS  (MCPWM_CLK_HZ / MCPWM_FREQ)
 #define     MCPWM_COUNT_MODE    MCPWM_TIMER_COUNT_MODE_UP   // 1, 2, 3, ... , 500, 1, 2, ...
 #define     MCPWM_GPIO          GPIO_NUM_25
+
+/* ====== PRIVATE VARIABLES ====== */
+
+static mcpwm_timer_handle_t timer_handle    = NULL;
+static mcpwm_oper_handle_t  oper_handle     = NULL;
+static mcpwm_cmpr_handle_t  cmpr_handle     = NULL;
+static mcpwm_gen_handle_t   gen_handle      = NULL;
 
 static const char *TAG = "motor_control";
 
 /* ====== EXPORTED FUNCTIONS ===== */
 
-void init_mcpwm(void)
+void mcpwm_init(void)
 {
     // Timer config => Sets the timing of other submodules
-    mcpwm_timer_handle_t timer_handle = NULL;
     mcpwm_timer_config_t timer_conf = {
         .group_id       = MCPWM_GROUP_ID,               // All the config needs to be done on the same group
         .clk_src        = MCPWM_CLK_SOURCE,        
         .resolution_hz  = MCPWM_CLK_HZ,                 // Sets the precision in which the duty cicle works
-        .period_ticks   = MCPWM_CLK_HZ / MCPWM_FREQ,    // Counts how many ticks fit in one period 
+        .period_ticks   = MCPWM_PERIOD_TICKS,           // Counts how many ticks fit in one period 
         .count_mode     = MCPWM_COUNT_MODE
     };
 
@@ -43,7 +50,6 @@ void init_mcpwm(void)
     ESP_LOGI(TAG, "timer config done");
 
     // Operator config => Responsible for generating the PWM waveforms => Connects timer with generators
-    mcpwm_oper_handle_t oper_handle = NULL;
     mcpwm_operator_config_t oper_conf = {
         .group_id = MCPWM_GROUP_ID
     };
@@ -52,10 +58,9 @@ void init_mcpwm(void)
     ESP_LOGI(TAG, "operator config done");
 
     // Connect timer with operator
-    mcpwm_operator_connect_timer(oper_handle, timer_handle);
+    ESP_ERROR_CHECK(mcpwm_operator_connect_timer(oper_handle, timer_handle));
 
     // Comparator config => Compares timer value with threshold to decide HIGH or LOW
-    mcpwm_cmpr_handle_t cmpr_handle = NULL;
     mcpwm_comparator_config_t cmpr_config = {
         .flags.update_cmp_on_tez = true                 // pwm only changes when ticks = 0 (Every period)
     };
@@ -64,7 +69,6 @@ void init_mcpwm(void)
     ESP_LOGI(TAG, "comparator config done");
 
     // Generator config => Connects with gpio
-    mcpwm_gen_handle_t gen_handle = NULL;
     mcpwm_generator_config_t gen_config = {
         .gen_gpio_num = MCPWM_GPIO
     };
@@ -80,7 +84,7 @@ void init_mcpwm(void)
                                      MCPWM_GEN_ACTION_HIGH),
 
         // When it gets to threshold it changes to low
-        MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_DOWN,
+        MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP,
                                         cmpr_handle,
                                         MCPWM_GEN_ACTION_LOW),
         
@@ -97,4 +101,19 @@ void init_mcpwm(void)
     mcpwm_timer_start_stop(timer_handle, MCPWM_TIMER_START_NO_STOP);
 
     ESP_LOGI(TAG, "MCPWM config done");
+}
+
+void mcpwm_set_duty(uint8_t per_duty)
+{
+    uint16_t threshold; 
+
+    if (per_duty > 100)
+    {
+        ESP_LOGE(TAG, "duty percentage error");
+        return;
+    }
+    
+    threshold = (uint16_t)(((uint32_t)per_duty * MCPWM_PERIOD_TICKS) / 100);
+    mcpwm_comparator_set_compare_value(cmpr_handle, threshold);
+    ESP_LOGI(TAG, "duty set succesfully");
 }
